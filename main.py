c6c2bb8c093e7e4348ffb724a69669c9e1dd90cc
Leolang <leolang99@gmail.com>
2022-06-24 23:12:51 -0300

    remade code

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-import warnings
-
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-warnings.filterwarnings('ignore')
+
 # lendo o dataset
 tic_tact_data = pd.read_csv('tic-tac-toe.csv')
-#convert all o values from the dataset to -1
+
 tic_tact_data.replace(to_replace='o', value=-1, inplace=True)
 tic_tact_data.replace(to_replace='b', value=0, inplace=True)
 tic_tact_data.replace(to_replace='x', value=1, inplace=True)
@@ -21,10 +17,10 @@ print(tic_tact_data.head())
 # salvando o dataset modificado
 tic_tact_data.to_csv('tic-tact-data2222.csv', index=False)
 # separando dataset
-X = tic_tact_data.iloc[:, :-1].values
-y = tic_tact_data.iloc[:, -1].values
+X = tic_tact_data.iloc[:,0:9]
+y = tic_tact_data.iloc[:,9]
 # separando dataset em treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, train_size=0.8)
 # criando o classificador
 classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
 # treinando o classificador
